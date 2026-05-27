b49ca4acefc9b909e720e56be81b99c7485ac326
Fercho524 <mrxd9767@gmail.com>
2026-05-27 11:48:17 -0600

    Correcciones del idioma

--- src/components/CTABanner.tsx
+++ src/components/CTABanner.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/link";
+import { Link } from "@/i18n/routing";
 import { ArrowRight, Sparkles } from "lucide-react";
 import { useTranslations } from "next-intl";
 
